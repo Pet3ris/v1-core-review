32bffebe5abcef59584d64ba5e21a467728c6d42
Peteris Erins <peteris.erins@gmail.com>
2022-04-14 11:34:08 +0100

    parent 825f426f553a561a25d0affb7238a6020521cc39 author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100 committer Peteris Erins <peteris.erins@gmail.com> 1649932657 +0100
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932655 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932654 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932652 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932650 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932645 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932642 +0100
    
    parent 825f426f553a561a25d0affb7238a6020521cc39
    author Peteris Erins <peteris.erins@gmail.com> 1649932448 +0100
    committer Peteris Erins <peteris.erins@gmail.com> 1649932636 +0100
    
    Empty commit
    
    add hardhat advanced typescript template
    
    add gnu3 license
    
    add base for technical spec
    
    add basic info
    
    update easyAuction strategy
    
    spelling
    
    spelling
    
    add flash loan draft
    
    update flash loans example
    
    add flash loan examples
    
    clarify flash loan examples
    
    Add auction spec
    
    Update spec/overview.md
    
    Co-authored-by: RusseII <russell@deephire.com>
    
    Update overview.md
    
    simplify tech spec - using aave as inspiration
    
    replace \ with double space
    
    https://www.markdownguide.org/basic-syntax/
    
    add section for detailing auction flow
    
    (feat) add simple bond contracts
    
    (fix) bond review :tada: (#20)
    
    * review fixes :tada:
    
    * add nonReentrant
    
    * BondStatusChange
    
    * pr fixes
    
    * add more tests !
    
    * organize the tests
    
    * use the bond standing enum
    
    Update bondToken.md
    
    (fix) only owner should call redeemBond()
    
    closes #24
    
    Add Auction and QaraghandyToken Contracts
    
    Add auction tests and its utilities
    
    add test config & vscode extension recommendation
    
    create fake erc20 tokens for testing
    
    update env example and change gas env key
    
    remove auction contracts
    
    (fix) add null state to bond
    
    closes #16
    
    (fix) only owner should call redeemBond() (#26)
    
    closes #24
    
    improve overview
    
    add more info about broker contract
    
    fix typos
    
    rename files
    
    Update overview.md
    
    middleperson to intermediary
    
    Update LICENSE
    
    Update repayment.md
    
    fix typo
    
    Update overview.md
    
    spelling
    
    add event emission catcher
    
    add more specific info about broker methods
    
    fix typos
    
    Update spec/Broker.md
    
    Co-authored-by: Kyle <kyletrusler@gmail.com>
    
    Update spec/Broker.md
    
    Co-authored-by: Kyle <kyletrusler@gmail.com>
    
    Update Broker.md
    
    Move to token pattern (#46)
    
    * update to token pattern
    
    * add part of user walkthrough
    
    * add cloneFactory design decision
    
    * Update overview.md
    
    * add new methods and add onto example
    
    * Update spec/overview.md
    
    Co-authored-by: RusseII <russell@deephire.com>
    
    * Update spec/overview.md
    
    Co-authored-by: RusseII <russell@deephire.com>
    
    Co-authored-by: bookland <bookland.porter.finance>
    Co-authored-by: Kyle <kyletrusler@gmail.com>

---
+++ scripts/deploy.ts
@@ -0,0 +1,36 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+import { ethers } from "hardhat";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  const weiAmount = (await deployer.getBalance()).toString();
+
+  console.log("Account balance:", await ethers.utils.formatEther(weiAmount));
+
+  const Token = await ethers.getContractFactory("SimpleBond");
+  // TODO: this is broken and needs fixed
+  const token = await Token.deploy();
+
+  console.log("Token address:", token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
